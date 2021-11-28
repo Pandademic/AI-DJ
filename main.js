7e459b1e4dfcff4fe88f4ffe8cbc5f86b00d4a2c
Pandademic <atharv.gupte510@gmail.com>
2021-11-27 20:18:41 -0500

    JS v 5

--- main.js
+++ main.js
@@ -53,7 +53,30 @@ function draw() {
 
 	fill("#FF0000");
 	stroke("#FF0000");
-	if(scoreLeftWrist > 0.2)
+	circle(rightWristX,rightWristY,20);
+	if(rightWristY>0 && rightWristY<=100 ){
+		document.getElementById("speed").innerHTML= "speed: 0.5x";
+		song.rate(0.5);
+	}
+	else if(rightWristY>100 && rightWristY<=200){
+		document.getElementById("speed").innerHTML="speed: 1x";
+		song.rate(1);
+	}
+	else if(rightWristY>200 && rightWristY<=300){
+		document.getElementById("speed").innerHTML="speed: 1.5x";
+		song.rate(1.5)
+	}
+	else if(rightWristY>300 && rightWristY<=400){
+		document.getElementById("speed").innerHTML="speed: 2x";
+		song.rate(2);
+	}
+	else if(rightWristY>400 && rightWristY<=500){
+		document.getElementById("speed").innerHTML="speed: 2.5x";
+		song.rate(2.5);
+		//TODO: find better way to search
+	}	
+
+	else if(scoreLeftWrist > 0.2)
 	{
 		circle(leftWristX,leftWristY,20);
 		InNumberleftWristY = Number(leftWristY);
